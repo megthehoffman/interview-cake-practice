d869da111b8945c21d674621f8a90a7abcb7377d
Megan Hoffman <meganhoffman@Megans-MacBook-Pro.local>
2018-10-14 16:36:00 -0700

    ALL TESTS PASSED

--- merge_ranges.py
+++ merge_ranges.py
@@ -7,6 +7,35 @@ def merge_ranges(meetings):
     [(0, 1), (3, 8), (9, 12)]
 
     """
+    sorted_meetings = sorted(meetings, key = lambda x: x[0])
+    # print(sorted_meetings)
+
+    i = 0
+    j = 1
+
+    current_tuple = sorted_meetings[i]
+    next_tuple = sorted_meetings[j]
+
+    condensed_meetings = []
+
+    while j < len(sorted_meetings):
+        if next_tuple[0] <= current_tuple[1]:
+            condensed_range = (current_tuple[0], next_tuple[1])
+            condensed_meetings.append(condensed_range)
+            i += 2
+            j += 2
+        else:
+            condensed_meetings.append(current_tuple)
+            i += 1
+            j += 1
+
+        if j < len(meetings):
+            current_tuple = sorted_meetings[i]
+            next_tuple = sorted_meetings[j]
+        
+        
+
+    return condensed_meetings
 
 
 if __name__ == "__main__":
