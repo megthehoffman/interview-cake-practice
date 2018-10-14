799ca7be8a63c84b4aa31c5fbea36563b3da6c03
Megan Hoffman <meganhoffman@Megans-MacBook-Pro.local>
2018-10-14 16:51:43 -0700

    Modified code for additional test cases

--- merge_ranges.py
+++ merge_ranges.py
@@ -3,15 +3,31 @@ def merge_ranges(meetings):
        Ranges are written in terms of 30-min blocks after 9am, but the function should be able to handle no upper bounds.
        EX: UNIX timestamps
 
+    Meeting times out of order
     >>> merge_ranges([(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
     [(0, 1), (3, 8), (9, 12)]
 
+    Duplicate meeting times
     >>> merge_ranges([(0, 1), (0, 1), (3, 5), (4, 8), (10, 12), (9, 10)])
     [(0, 1), (3, 8), (9, 12)]
 
+    Subsumed meeting times
+    >>> merge_ranges([(1, 5), (2, 3)])
+    [(1, 5)]
+
+    Connected meeting times
+    >>> merge_ranges([(1, 2), (2, 3)])
+    [(1, 3)]
+
+    Result of merge may need to be merged with next
+    >>> merge_ranges([(1, 10), (2, 6), (3, 5), (7, 9)])
+    [(1, 10)]
+
+    One meeting time
     >>> merge_ranges([(0, 1)])
     [(0, 1)]
 
+    No meeting times
     >>> merge_ranges([])
     []
 
@@ -31,7 +47,11 @@ def merge_ranges(meetings):
     condensed_meetings = []
 
     while j < len(sorted_meetings):
-        if next_tuple[0] <= current_tuple[1]:
+        if next_tuple[0] <= current_tuple[1] and next_tuple[1] <= current_tuple[1]:
+            condensed_meetings.append(current_tuple)
+            i += 2
+            j += 2
+        elif next_tuple[0] <= current_tuple[1]:
             condensed_range = (current_tuple[0], next_tuple[1])
             condensed_meetings.append(condensed_range)
             i += 2
