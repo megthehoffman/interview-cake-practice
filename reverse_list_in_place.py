fe218c9c66a698369142db138d8d50739a75f9fe
Megan Hoffman <meganhoffman@Megans-MacBook-Pro.local>
2018-10-15 10:58:04 -0700

    Completed in place reversal using several techniques

--- reverse_list_in_place.py
+++ reverse_list_in_place.py
@@ -1,22 +1,47 @@
 def reverse_list_in_place(lst):
     """Write a function that takes a list of characters and reverses the list in place.
 
-    >>> reverse_list_in_place(['c','a','t'])
-    ['t','a','c']
+    >>> reverse_list_in_place(['c', 'a', 't'])
+    ['t', 'a', 'c']
 
-    >>> reverse_list_in_place(['r','a','c','e','c','a','r'])
-    ['r','a','c','e','c','a','r']
+    >>> reverse_list_in_place(['r', 'a', 'c', 'e', 'c', 'a', 'r'])
+    ['r', 'a', 'c', 'e', 'c', 'a', 'r']
 
-    >>> reverse_list_in_place(['h','i'])
-    ['i','h']
+    >>> reverse_list_in_place(['h', 'i'])
+    ['i', 'h']
 
     >>> reverse_list_in_place(['z'])
     ['z']
 
     >>> reverse_list_in_place([])
     []
-    
+
     """
+
+    # OPTION 1: reverse() method, returns an iterator in place
+    lst.reverse()
+
+    return lst
+
+
+    # OPTION 2: reversed() function, returns an iterable in place
+    reversed_lst = []
+
+    for x in reversed(lst):
+        reversed_lst.append(x)
+
+    return reversed_lst
+
+
+    # OPTION 3: reversed() function, listified
+
+    return list(reversed(lst))
+
+
+    # OPTION 4: list slicing, [start:stop:step]
+
+    return lst[::-1]
+
 
 if __name__ == "__main__":
     import doctest
